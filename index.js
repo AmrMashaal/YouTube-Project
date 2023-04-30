a554278891697c9be1dcf05dadd32d4f0c603390
AmrMashaal <115498987+AmrMashaal@users.noreply.github.com>
2023-04-30 06:31:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,3 +40,14 @@ behind.onclick = function () {
   behind.classList.remove("ds-block");
   wrLeft.classList.remove("return");
 };
+
+let box = document.querySelectorAll(".right2 .box");
+
+box.forEach(function (e){
+  e.addEventListener("mousedown", function (){
+    e.style.backgroundColor = "#ffffff1c"
+  })
+  e.addEventListener("mouseup", function (){
+    e.style.backgroundColor = "transparent"
+  })
+})
